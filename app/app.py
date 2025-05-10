533e1401045086159e7ab7ef4e5baf7763487ed7
Maialen Tolosa <s_tolosa24@stud.hwr-berlin.de>
2025-05-10 17:36:47 +0200

    Docker Exercise

--- app/app.py
+++ app/app.py
@@ -1,8 +1,12 @@
 import time
 import redis
 from flask import Flask, render_template
+import os
+from dotenv import load_dotenv
+import pandas as pd
 
-cache = redis.Redis(host='redis', port=6379)
+load_dotenv() 
+cache = redis.Redis(host=os.getenv('REDIS_HOST'), port=6379,  password=os.getenv('REDIS_PASSWORD'))
 app = Flask(__name__)
 
 def get_hit_count():
@@ -20,6 +24,17 @@ def get_hit_count():
 def hello():
     count = get_hit_count()
     return render_template('hello.html', name= "BIPM", count = count)
+
+@app.route('/titanic')
+def titanic():
+    df = pd.read_csv('titanic.csv')  # adjust path if needed
+    table_html = df.head().to_html(classes='data', header="true")
+    gender_survival = df[df['survived'] == 1]['sex'].value_counts().to_dict()
+    return render_template('titanic.html', table=table_html,chart_data=gender_survival)
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80, debug=True)
